e3bb103552f6b7fd0834ce63b43f4f855daa8c56
Chris <chrisanthony97@hotmail.com>
2019-10-08 10:29:42 -0500

    Updated Changes

--- assignments/homework_2/main.cpp
+++ assignments/homework_2/main.cpp
@@ -1,45 +1,226 @@
-/**
- * File: main.cpp
- * Chris Anthony
- * Course: CMPS 1063 - Fall 2019
- * Date: 24 Sep 24
- * Description:
- *    Main driver of an Array based implementation of a stack.
- */
+/** Chris Anthony
+Data Structures : Dr. Griffin
+Homework 2 - Resizable Array Stack
+Description:This program reads data from a file and utilizes three methods
+to check, enlarge and shrink the size an array based stack. The results
+are printed to an output file.
+*/
 
-#include <iostream>
-#include "stack.h"
-#include "stdlib.h"
+#include<iostream>
+#include<fstream>
 
 using namespace std;
 
+class Stack
+{
+private:
+	int *A;         // container of items (could make it a list)
+	int Top;        // keep track of top
+	int Size;       // Arrays need a size
+	int count;
+
+public:
+
+	/**
+	 * Stack constructor (default)
+	 * Description:
+	 *    Inits an array of ints and sets our top
+	 */
+
+	Stack()
+	{
+		Size = 10;
+		A = new int[Size];
+		Top = -1;
+		count = Size;
+	};
+
+	/**
+	 * Stack constructor (overloaded)
+	 * Description:
+	 *    Inits an array of ints to a specified size and sets our top
+	 * Params:
+	 *    int s : integer size
+	 */
+
+	Stack(int s)
+	{
+		Size = s;
+		A = new int[Size];
+		Top = -1;
+	};
+
+	/**
+	 * Push
+	 * Description:
+	 *    Adds item to top of stack
+	 * Params:
+	 *    int val : integer size
+	 * Returns:
+	 *     bool : true = success
+	 */
+
+	bool Push(int val)	// Push value onto stack
+	{
+		if (!Full()) {
+			Top++;
+			A[Top] = val;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Pop
+	 * Description:
+	 *    Removes top of stack and returns it
+	 * Params:
+	 *    void
+	 * Returns:
+	 *     int : item on stack
+	 */
+
+	int Pop()	// Remove item from top of stack
+	{
+		if (!Empty()) {
+			int temp = 0;
+			temp = A[Top];
+			Top--;
+			return temp;
+		}
+		else {
+			// should return a value that implies failuer, but good enough for now
+			cout << "Cannot remove item from empty stack" << endl;
+		}
+		return 0;
+	}
+
+	/**
+	 * Empty
+	 * Description:
+	 *    Is stack empty
+	 * Params:
+	 *    void
+	 * Returns:
+	 *    bool : true = empty
+	 */
+
+	bool Empty()
+	{
+		return Top < 0;
+	}
+
+	/**
+	 * Full
+	 * Description:
+	 *    Is stack full
+	 * Params:
+	 *    void
+	 * Returns:
+	 *    bool : true = full
+	 */
+
+	bool Full()
+	{
+		return (Top == Size - 1);
+	}
+
+	//void checkResize(int newSize)
+	{
+
+		if (count = .8 * Size)
+		{
+			Enlarge();
+		}
+		else (count = .2 * newSize)
+		{
+			Reduce();
+		}
+	}
+
+	//void Enlarge()
+	{
+		int newSize = Size * 1.5;
+		int *B = new int[newSize];
+		int *deleter = NULL;
+
+		for (int i = 0; i < Size; i++)
+		{
+			B[i] = A[i];
+		}
+
+		for (int i = Size; i <= newSize; i++)
+		{
+			B[i] = NULL;
+		}
+
+		deleter = A;
+		A = B;
+		delete[] deleter;
+	}
+
+	//void Reduce()
+	{
+		int oldSize = Size;
+		int *C = new int(oldSize);
+		int *deleter1 = NULL;
+
+
+		for (int i = 0; i < oldSize; i++)
+		{
+			C[i] = B[i];
+		}
+
+		deleter1 = B;
+		B = C;
+		delete[] deleter1;
+	}
+	/**
+ * Print
+ * Description:
+ *    Prints stack for inspection
+ * Params:
+ *    void
+ * Returns:
+ *     void
+ */
+
+	void Print() // function to print the contents of the list
+	{
+		for (int i = Top; i >= 0; i--) {
+			cout << A[i] << endl;
+		}
+	}
+};
+
 /**
  * Main Program
- * 
+ *
  */
-int main() {
-  srand(34234); // seed random number gen
+int main() 
+{
+	ofstream outfile;
+	outfile.open("stack_out.dat");
 
-  Stack S1; // Instance of our stack default constructor
-  Stack S2(200);  // Instance of our stack specified size
+	ifstream fin;
+	fin.open("input_data.txt");
 
-  // Load the stack with random nums
-  for (int i = 0; i < 10; i++) {
-    S1.Push(rand() % 100);
-  }
+	Stack S; // Instance of our stack default constructor
+	int values[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int val;
 
-  // make sure it looks correct
-  S1.Print();
+	// Load the stack with data from file
+	for (int i = 0; i < 10; i++)
+	{
+		s.Push(values[i]);
+	}
 
-  // Pop some items off the top  
-  S1.Pop();
-  S1.Pop();
-  S1.Pop();
-  S1.Pop();
+	cout << values[i] << endl;
 
-  cout << endl;
-
-  // Check for correct behavior again.
-  S1.Print();
-  return 0;
+	system("pause");
+	return 0;
 }
+
